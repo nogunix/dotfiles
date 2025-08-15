867f9b724e4821d0a031decb7e5f3faa66d475de
Nogunix <nogunix@gmail.com>
2025-08-15 20:46:07 +0900

    feat: initial import: zsh, nvim, tmux

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,197 @@
+-- vim options
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.wo.number = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.background = 'dark'
+vim.opt.showmatch = true
+vim.opt.matchtime = 1
+-- vim.cmd("set mouse=") -- マウスを有効にする場合はコメントを外してください
+vim.cmd('filetype plugin indent on') -- ファイルタイプ検出、プラグイン、インデント機能を有効にする
+vim.cmd('syntax on')                -- シンタックスハイライトを有効にする
+vim.opt.title = true                -- ターミナルタイトルバーにファイル名を表示する
+
+---
+-- ## プラグイン管理 (`lazy.nvim`)
+
+-- ```lua
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    [https://github.com/folke/lazy.nvim](https://github.com/folke/lazy.nvim)
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup({
+  -- NOTE: First, some plugins that don't require any configuration
+  { 'lervag/vimtex' },
+  { 'nvim-tree/nvim-web-devicons' },
+  { 'github/copilot.vim' },
+  { 'h-hg/fcitx.nvim' },
+  {
+    "nogunix/vim-lsdyna",
+    event = { "BufReadPre *.k", "BufReadPre *.key", "BufNewFile *.k", "BufNewFile *.key" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = { "c", "lua", "rust" },
+        highlight = { enable = true, }
+      }
+    end
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, { { name = 'buffer' } })
+      })
+    end
+  },
+
+  -- Theme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme 'tokyonight'
+    end
+  },
+
+  -- LSP and Mason
+  {
+    'williamboman/mason.nvim',
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      require("mason").setup {}
+      local mason_lspconfig = require("mason-lspconfig")
+      local on_attach = function(_, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, "formatexpr",
+          "v:lua.vim.lsp.formatexpr(#{timeout_ms:250})")
+        -- 必要であれば、ここにlsp_onattach_funcのようなグローバル関数を定義して呼び出せます。
+        -- _G.lsp_onattach_func(i, bufnr)
+      end
+
+      -- ここに自動でインストールしたいLSPサーバーを追加します
+      -- `ensure_installed`に自動インストールしたいサーバーを追加できます。
+      -- 例: ensure_installed = { "lua_ls", "rust_analyzer", "omnisharp" }
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls" }, -- この設定ファイル自体を編集するためにlua_lsを追加
+      })
+
+      local lspconfig = require("lspconfig")
+
+      -- サーバーごとのカスタム設定
+      local lua_settings = {
+        settings = {
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            diagnostics = { globals = { 'vim' } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file('', true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      }
+      local server_settings = {
+        lua_ls = lua_settings,
+        -- 古いサーバー名に対応 (sumneko_lua)
+        sumneko_lua = lua_settings,
+        omnisharp = { useGlobalMono = "always" },
+        -- 他のサーバー設定をここに追加
+        -- rust_analyzer = { ... }
+      }
+
+      -- インストール済みのサーバーをセットアップ
+      for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
+        local opts = { on_attach = on_attach }
+        -- カスタム設定があればマージする
+        if server_settings[server_name] then
+          opts = vim.tbl_deep_extend("force", opts, server_settings[server_name])
+        end
+        lspconfig[server_name].setup(opts)
+      end
+      -- vim.cmd("LspStart") -- Masonとlspconfigがサーバーを管理するため、このコマンドは通常不要です。
+    end,
+  },
+    -- Lualine (Statusline)
+    require('plugins.lualine'),
+  -- Markdown Preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  }
+})
+-- Viewer options: One may configure the viewer either by specifying a built-in
+-- viewer method:
+vim.g.vimtex_view_method = 'zathura'
+
+-- Or with a generic interface:
+vim.g.vimtex_view_general_viewer = 'evince'
+vim.g.vimtex_view_general_options = {
+  unique = true,
+  file = '@pdf',
+  src = '@line@tex'
+}
+
+-- VimTeX uses latexmk as the default compiler backend. If you use it, which is
+-- strongly recommended, you probably don't need to configure anything. If you
+-- want another compiler backend, you can change it as follows. The list of
+-- supported backends and further explanation is provided in the documentation,
+-- see ":help vimtex-compiler".
+vim.g.vimtex_compiler_method = 'latexmk'
+
+-- Most VimTeX mappings rely on localleader and this can be changed with the
+-- following line. The default is usually fine and is the symbol "\".
+-- vim.cmd('let maplocalleader = ", "')
---
+++ zsh/.zshrc
@@ -0,0 +1,102 @@
+# Zshの起動を高速化するために、.zshrcが更新された場合のみコンパイルする
+if [[ -f ~/.zshrc && ~/.zshrc -nt ~/.zshrc.zwc ]]; then
+  zcompile ~/.zshrc
+fi
+
+#==============================================================================
+# History
+#==============================================================================
+HISTFILE=$HOME/.zsh_history     # 履歴を保存するファイル
+HISTSIZE=100000                 # メモリ上に保存する履歴のサイズ
+SAVEHIST=1000000                # 上述のファイルに保存する履歴のサイズ
+
+setopt inc_append_history       # 実行時に履歴をファイルにに追加していく
+setopt share_history            # 履歴を他のシェルとリアルタイム共有する
+setopt hist_ignore_all_dups     # ヒストリーに重複を表示しない
+setopt hist_save_no_dups        # 重複するコマンドが保存されるとき、古い方を削除する。
+setopt extended_history         # コマンドのタイムスタンプをHISTFILEに記録する
+setopt hist_expire_dups_first   # HISTFILEのサイズがHISTSIZEを超える場合は、最初に重複を削除します
+
+#==============================================================================
+# Completion
+#==============================================================================
+autoload -Uz compinit; compinit
+autoload -Uz colors; colors
+
+# Tabで選択できるようにする
+zstyle ':completion:*:default' menu select=2
+# 補完で大文字小文字を区別しない
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# ファイル補完候補に色を付ける
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+#==============================================================================
+# Shell Options
+#==============================================================================
+setopt auto_param_slash       # ディレクトリ名の補完で末尾の / を自動的に付加
+setopt auto_param_keys        # カッコを自動補完
+setopt mark_dirs              # ファイル名の展開でディレクトリにマッチした場合 末尾に / を付加
+setopt auto_menu              # 補完キー連打で順に補完候補を自動で補完
+# setopt correct              # スペルミス訂正 (無効)
+setopt interactive_comments   # コマンドラインでも # 以降をコメントと見なす
+setopt magic_equal_subst      # --prefix=/usr などの = 以降でも補完できる
+setopt complete_in_word       # 語の途中でもカーソル位置で補完
+setopt print_eight_bit        # 日本語ファイル名を表示可能にする
+setopt auto_cd                # ディレクトリ名だけでcdする
+setopt no_beep                # ビープ音を消す
+
+#==============================================================================
+# Keybindings
+#==============================================================================
+# コマンドを途中まで入力後、historyから絞り込み (Ctrl+P/Ctrl+N)
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^P" history-beginning-search-backward-end
+bindkey "^N" history-beginning-search-forward-end
+
+
+#==============================================================================
+# Zinit (Plugin Manager)
+#==============================================================================
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+# --- Plugins ---
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light rupa/z
+### End of Zinit's installer chunk
+
+#==============================================================================
+# Aliases & Exports
+#==============================================================================
+# --- Aliases ---
+# クリップボードへのコピー。
+# Wayland/X11環境を自動で判別し、適切なコマンド (wl-copy/xclip) を使います。
+if [[ -n "$WAYLAND_DISPLAY" ]]; then
+  alias clip='wl-copy'
+else
+  alias clip='xclip -selection c'
+fi
+
+# --- Exports ---
+export LIBVA_DRIVER_NAME=radeonsi
